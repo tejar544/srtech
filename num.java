6531a73d2651a4b6ba88424813b3f9c803c42026
root <root@ip-172-31-17-4.eu-north-1.compute.internal>
2025-04-12 16:26:59 +0000

    i changed

---
+++ num.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class num {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+
+        scanner.close();
+    }
+}
+
